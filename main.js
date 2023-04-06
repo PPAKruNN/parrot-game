fdfe5432c8fb93b700bd9450a2cb8f7d9639f245
Pedro Nascimento <sn.jaypee@gmail.com>
2023-04-06 11:09:31 -0300

    Virar carta criada

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function virarCarta() {
+    console.log("virar carta.")
+    const frente = document.querySelector(".frente")
+    const atras = document.querySelector(".atras")
+
+    if(atras.style.transform == "rotateY(0deg)"){
+        virarParaFrente(frente, atras)
+    } else {
+        virarParaTras(frente,atras)
+    }
     
+
+}
+
+function virarParaTras(frente, atras) {
+    frente.style.transform = "rotateY(-180deg)"    
+    atras.style.transform = "rotateY(0deg)"
+}
+
+function virarParaFrente(frente, atras) {
+    frente.style.transform = "rotateY(0deg)"    
+    atras.style.transform = "rotateY(180deg)"
 }
